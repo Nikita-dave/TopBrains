8e4b13a8b789446cd6e1190b20cd6d818593dcb8
Nikita-dave <davenikita2003@gmail.com>
2026-01-20 11:53:01 +0530

    code

---
+++ MidPointRounding/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter radius: ");
+        double radius = double.Parse(Console.ReadLine());
+        double area = Math.PI * radius*radius;
+        Console.WriteLine("Area: {0:F2}",area);
+    }
+}
