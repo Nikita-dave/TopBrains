792a2168653b90b5037e485cbd2018a952950136
Nikita-dave <davenikita2003@gmail.com>
2026-01-20 12:37:18 +0530

    code

---
+++ LargestInteger/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public static int LargestInteger(int a, int b, int c)
+    {
+        if(a>b && a > c)
+        {
+            return a;
+        }else if(b>a && b > c)
+        {
+            return b;
+        }
+        else
+        {
+            return c;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter Numbers: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine(LargestInteger(a,b,c));
+    }
+}
