b353e4a43dc369fb35ed7a64f0e1fb2dcc0403f8
Nikita-dave <davenikita2003@gmail.com>
2026-01-20 12:20:09 +0530

    code

---
+++ DisplayHeight/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    public static string DisplayHeight(int height)
+    {
+        if(height < 150)
+        {
+            return "Short";
+        }
+        else if(height>=150 && height<180)
+        {
+            return "Average";
+        }
+        else
+        {
+            return "Tall";
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter height (0 to 300):");
+        int height = int.Parse(Console.ReadLine());
+        System.Console.WriteLine(DisplayHeight(height));
+    }
+}
