4b59122cedaeaaca34bd4a673e041ea45c95e3a0
Nikita-dave <davenikita2003@gmail.com>
2026-01-20 12:08:57 +0530

    code

---
+++ Conversion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    public static double Conversion(int feet)
+    {
+        double centimeters = 30.48*feet;
+        return Math.Round(centimeters, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter value in Feet: ");
+        int feet = int.Parse(Console.ReadLine());
+        System.Console.WriteLine(Conversion(feet));
+    }
+}
