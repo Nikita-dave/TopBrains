6af77f258b0002832021ac626ee21079ecb2c2d8
Nikita-dave <davenikita2003@gmail.com>
2026-01-20 15:38:03 +0530

    code

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program{
+   public static int[] MultiplicationTable(int n, int upto)
+    {
+        int[] result = new int[upto];
+        for(int i = 1; i <= upto; i++)
+        {
+            result[i-1] = n*i;
+        }  
+        return result;
+    }
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int upto = Convert.ToInt32(Console.ReadLine());
+
+        int[] result = MultiplicationTable(n, upto);
+
+        foreach (int item in result)
+        {
+            System.Console.Write(item+" ");
+        }
+    } 
+}
